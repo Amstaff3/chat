59609e1254437f325e64ce663c0dd841ad4c892a
Amstaff3 <amstaff.srbija@gmail.com>
2023-06-19 18:55:28 +0200

    the change description

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,35 @@
-# Verwende das offizielle Node.js-Image als Basisimage
-FROM node:18
+# Offizielles Node.js-Image als Basis
+FROM node:latest
 
-# Setze das Arbeitsverzeichnis innerhalb des Containers
+# Arbeitsverzeichnis im Container
 WORKDIR /app
 
-# Kopiere die package.json und package-lock.json Dateien in den Container
+# Kopiert die package.json und package-lock.json in den Container
 COPY package.json package-lock.json ./
+
+# Installiert die Abhängigkeiten
+RUN apt-get update && \
+    apt-get install -y nginx && \
+    npm install -g ws uuid redis nodemon livereload connect-livereload 
 
 # Installiere die Abhängigkeiten
 RUN yarn install
 
-# Kopiere den Quellcode in den Container
+# Manuelle Installation von 'express'
+RUN npm install express
+
+# Kopiert den Rest des Codes in den Container
 COPY . .
+COPY websocketserver.js /app
+COPY app.js /app
 
-# Starte den Server beim Starten des Containers
+# Startskript für den Webserver
+COPY start.sh /start.sh
+RUN chmod +x /start.sh
+
+# Setzt den Port, den die Anwendung im Container öffnet
+EXPOSE 3000
+EXPOSE 80
+
+# Starte die Anwendung beim Start des Containers
 CMD ["yarn", "start"]
-
---
+++ websocketserver.js
@@ -0,0 +1,240 @@
+
+//Server_Code// zeigt chats korrekt an!!!!
+const WebSocket = require("ws");
+const redis = require("redis");
+
+let publisher;
+const clients = [];
+
+// Initialisieren des Websocket-Servers
+const initializeWebsocketServer = async (server) => {
+  const client = redis.createClient({
+    socket: {
+      host: process.env.REDIS_HOST || "localhost",
+      port: process.env.REDIS_PORT || "6379",
+    },
+  });
+
+  const subscriber = client.duplicate();
+  await subscriber.connect();
+  publisher = client.duplicate();
+  await publisher.connect();
+
+  const websocketServer = new WebSocket.Server({ server });
+  websocketServer.on("connection", onConnection);
+  websocketServer.on("error", console.error);
+  await subscriber.subscribe("newMessage", onRedisMessage);
+  await publisher.publish("newMessage", "Hello from Redis!");
+};
+
+const onConnection = (ws) => {
+  console.log("New websocket connection");
+  ws.on("close", () => onClose(ws));
+  ws.on("message", (message) => onClientMessage(ws, message));
+  ws.send("Hello Teko Client!");
+  clients.push(ws);
+};
+
+const onClientMessage = (ws, message) => {
+  console.log("Message received: " + message);
+  publisher.publish("newMessage", message);
+};
+
+const onRedisMessage = (message) => {
+  console.log("Redis Message received: " + message);
+  clients.forEach((client) => client.send(message));
+};
+
+const onClose = (ws) => {
+  console.log("Websocket connection closed");
+  const index = clients.indexOf(ws);
+  if (index !== -1) {
+    clients.splice(index, 1);
+  }
+};
+
+module.exports = { initializeWebsocketServer };
+
+//-------------------------------------------------------------------------------------------//
+
+//Test probier eine userlist zu erstellen geht nicht //
+/*const WebSocket = require("ws");
+const redis = require("redis");
+
+let publisher;
+const clients = [];
+const activeUsers = new Set();
+
+const initializeWebsocketServer = async (server) => {
+  const client = redis.createClient({
+    socket: {
+      host: process.env.REDIS_HOST || "localhost",
+      port: process.env.REDIS_PORT || "6379",
+    },
+  });
+
+  const subscriber = client.duplicate();
+  await subscriber.connect();
+  publisher = client.duplicate();
+  await publisher.connect();
+
+  const websocketServer = new WebSocket.Server({ server });
+  websocketServer.on("connection", onConnection);
+  websocketServer.on("error", console.error);
+  await subscriber.subscribe("newMessage", onRedisMessage);
+  await publisher.publish("newMessage", "Hello from Redis!");
+};
+
+const onConnection = (ws) => {
+  console.log("New websocket connection");
+  ws.on("close", () => onClose(ws));
+  ws.on("message", (message) => onClientMessage(ws, message));
+  ws.send("Hello Teko Client!");
+
+  // Füge den Benutzer zur Liste der aktiven Benutzer hinzu
+  activeUsers.add(ws);
+
+  // Sende die aktualisierte Benutzerliste an alle Clients
+  broadcastActiveUsers();
+};
+
+const onClientMessage = (ws, message) => {
+  console.log("Message received: " + message);
+  publisher.publish("newMessage", message);
+};
+
+const onRedisMessage = (message) => {
+  console.log("Redis Message received: " + message);
+  clients.forEach((client) => client.send(message));
+};
+
+const onClose = (ws) => {
+  console.log("Websocket connection closed");
+  const index = clients.indexOf(ws);
+  if (index !== -1) {
+    clients.splice(index, 1);
+  }
+
+  // Entferne den Benutzer aus der Liste der aktiven Benutzer
+  activeUsers.delete(ws);
+
+  // Sende die aktualisierte Benutzerliste an alle Clients
+  broadcastActiveUsers();
+};
+
+const broadcastActiveUsers = () => {
+  const activeUsersArray = Array.from(activeUsers).map((ws) => ws.getClientName());
+  clients.forEach((client) => client.send(JSON.stringify({ type: "userList", payload: activeUsersArray })));
+};
+
+
+module.exports = { initializeWebsocketServer };
+*/
+
+//--------------------------------------------------------------------------------------------//
+
+/*
+const WebSocket = require("ws");
+const redis = require("redis");
+let publisher;
+
+const clients = [];
+
+// Initialisieren des Websocket-Servers
+const initializeWebsocketServer = async (server) => {
+  const client = redis.createClient({
+    socket: {
+      host: process.env.REDIS_HOST || "localhost",
+      port: process.env.REDIS_PORT || "6379",
+    },
+  });
+  // Dies ist der Abonnententeil
+  const subscriber = client.duplicate();
+  await subscriber.connect();
+  // Dies ist der Publisher-Teil
+  publisher = client.duplicate();
+  await publisher.connect();
+
+  const websocketServer = new WebSocket.Server({ server });
+  websocketServer.on("connection", onConnection);
+  websocketServer.on("error", console.error);
+  await subscriber.subscribe("newMessage", onRedisMessage);
+  await publisher.publish("newMessage", "Hello from Redis!");
+};
+
+// Wenn eine neue Verbindung aufgebaut wird, wird die onConnection-Funktion aufgerufen
+const onConnection = (ws) => {
+  console.log("New websocket connection");
+  ws.on("close", () => onClose(ws));
+  ws.on("message", (message) => onClientMessage(ws, message));
+  ws.send("Hello Client!");
+  clients.push(ws); // Client zum Array hinzu //
+  // TODO!!!!!! Fügen Sie den Client zum clients-Array hinzu
+};
+
+// Wenn eine neue Nachricht empfangen wird, wird die onClientMessage-Funktion aufgerufen
+const onClientMessage = (ws, message) => {
+  console.log("Message received: " + message);
+  publisher.publish("newMessage", message); // Sende Nachricht an den Redis-Kanal //
+  //TODO!!!!!! Senden Sie die Nachricht an den Redis-Kanal
+};
+
+// Wenn eine neue Nachricht aus dem Redis-Kanal empfangen wird, wird die Funktion onRedisMessage aufgerufen
+const onRedisMessage = (message) => {
+  console.log("Message received: " + message);
+  clients.forEach(client => client.send(message)); // Senden Nachricht an alle Clients //
+  //TODO!!!!!! Senden Sie die Nachricht an alle verbundenen Clients
+};
+
+// Wenn eine Verbindung geschlossen wird, wird die onClose-Funktion aufgerufen
+const onClose = (ws) => {
+  console.log("Websocket connection closed");
+  const index = clients.indexOf(ws);
+  if (index !== -1) {
+    clients.splice(index, 1); // Entfernen den Client aus Array //
+  }
+  //TODO!!!!!! Entfernen Sie den Client aus dem clients-Array
+};
+
+// Implementiren vom Namen //
+// Elemente zum Anzeigen des Clientnamens und der Chat-Nachrichten //
+const clientNameInput = document.getElementById("clientName");
+const chatMessages = document.getElementById("chatMessages");
+
+// Event-Listener für das Senden einer Nachricht über die WebSocket-Verbindung //
+document.getElementById("send1").addEventListener("click", function () {
+  const clientName = clientNameInput.value;
+  const messageInput = document.getElementById("message1");
+  const message = messageInput.value;
+
+  // Überprüfen, ob sowohl der Clientname als auch die Nachricht vorhanden sind //
+  if (clientName && message) {
+    // Nachricht formatieren: "[Clientname]: [Nachricht]" //
+    const formattedMessage = `${clientName}: ${message}`;
+
+    // Nachricht an den Server senden
+    socket.send(formattedMessage);
+    messageInput.value = "";
+
+    // Anzeigen der gesendeten Nachricht auf der Webseite //
+    displayMessage(formattedMessage);
+  }
+});
+
+// Event-Listener für das Empfangen von Nachrichten über die WebSocket-Verbindung //
+socket.addEventListener("message", (event) => {
+  const message = event.data;
+  displayMessage(message);
+});
+
+// Funktion zum Anzeigen der Nachricht auf der Webseite //
+function displayMessage(message) {
+  const messageElement = document.createElement("div");
+  messageElement.textContent = message;
+  chatMessages.appendChild(messageElement);
+}
+
+
+module.exports = { initializeWebsocketServer };
+*/
+
